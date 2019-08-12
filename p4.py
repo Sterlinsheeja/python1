da6576134535eee7dc7b604db6e8414ac91a9cc1
Sterlinsheeja <52779700+Sterlinsheeja@users.noreply.github.com>
2019-08-12 01:28:13 -0900

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,1 +1,10 @@
+a=int(input("enter the no:"))
+b=int(input("enter the no:"))
+c=int(input("enter the no:"))
+if(a>b)and(a>c):
+    print("a is greater")
+elif(b>a)and(b>c):
+    print("b is greater")
+else:
+    print("c is greater")
 
