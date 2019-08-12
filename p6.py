35c2675e29346c3423f56bb2d915fb1add5c9bbe
Sterlinsheeja <52779700+Sterlinsheeja@users.noreply.github.com>
2019-08-12 22:33:30 +0530

    Update p6.py

--- p6.py
+++ p6.py
@@ -1,1 +1,16 @@
+def reverse(s): 
+    return s[::-1]
+def isPalindrome(s): 
+    rev = reverse(s)
+    if (s == rev): 
+        return True
+    return False
+s = "sheeja"
+ans = isPalindrome(s) 
+if ans == 1: 
+    print("Yes") 
+else: 
+    print("No") 
+
+  
 
