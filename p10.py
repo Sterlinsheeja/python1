c02dccd1cf4dcbd82e9b643beb095342baa0d694
Sterlinsheeja <52779700+Sterlinsheeja@users.noreply.github.com>
2019-08-13 20:37:03 -0900

    Update p10.py

--- p10.py
+++ p10.py
@@ -1,1 +1,9 @@
-
+a=int(input("enter the no:"))
+b=int(input("enter the no:"))
+c=int(input("enter the no:"))
+sum=a+b+c
+print("sum:",sum)
+if(a==b==c):
+    print(sum*3)
+else:
+    print("false")
